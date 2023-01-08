821d48b2ad364642e9025720eec31ad99ffe16e3
pratyush-45 <pratyush4599@gmail.com>
2023-01-08 13:46:53 +0530

    OAuth Credentials added

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "https://secrets-z37a.onrender.com/auth/google/secrets",
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
